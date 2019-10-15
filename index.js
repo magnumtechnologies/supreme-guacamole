5ead69aaa14600c547a35697a94cbcae0ac3b8c8
alap <alap@themagnum.in>
2019-10-15 16:06:26 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express()
+const fs = require('fs-extra');
+var logger = require('tracer').dailyfile({root:'/var/log/foo/', maxLogFiles: 10, allLogsFileName: 'foo'});
+const counterFile = "/usr/src/foo/counter.json";
+
+app.get('/', (req,res)=>{incrementCounter(()=>res.sendFile(counterFile))})
+app.listen(8080, ()=>console.log("Running at 8080"))
+
+async function incrementCounter(cb){
+	try{
+		fs.pathExists(counterFile) 
+		.then(exists =>{
+			if (exists){
+				fs.readJson(counterFile)
+				.then(counterObj=>{
+					fs.writeJson(counterFile, {count: parseInt(counterObj.count)+1})
+					.then(()=>cb())
+					.catch(e=>logger.error("counterFile increment error: "+e));
+				})
+				.catch(e=>logger.error("counterFile read error: "+e));
+			}else{
+				fs.writeJson(counterFile, {count: 1})
+				.catch(e=>logger.error("counterFile write error: "+e));
+			}
+		})
+		.catch(e => {
+			logger.error("counterFile error: "+e);
+		})
+		
+	}
+	catch(e){
+		logger.error("incrementCounter exception: "+e);
+	}
+}
