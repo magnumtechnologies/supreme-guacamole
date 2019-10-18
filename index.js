1ce66bd43691fb966e7e0cc4276ef5b8b76956da
alap <alap@themagnum.in>
2019-10-18 19:32:26 +0530

    Added aws-sdk. Logs every request count with cloudwatch

--- index.js
+++ index.js
@@ -2,9 +2,33 @@ const express = require('express');
 const app = express()
 const fs = require('fs-extra');
 var logger = require('tracer').dailyfile({root:'/var/log/foo/', maxLogFiles: 10, allLogsFileName: 'foo'});
+var aws = require('aws-sdk');
+var cloudwatch = new aws.CloudWatch();
 const counterFile = "/usr/src/foo/counter.json";
 
-app.get('/', (req,res)=>{incrementCounter(()=>res.sendFile(counterFile))})
+var requestCount = {
+	MetricData: [
+		{
+			MetricName: 'RequestsHandled',
+			Dimensions: [
+				{
+					Name: 'RouteName',
+					Value: 'Root'
+				}
+			],
+			StorageResolution: '1',
+			Timestamp: new Date().toISOString(),
+			Unit: 'Count',
+			Value: '1',
+		}
+	],
+	Namespace: 'FooBar/'
+}
+
+app.get('/', (req,res)=>{
+	cloudwatch.putMetricData(requestCount, (e,d)=>{if(e)logger.error("cloudwatch put metric data error: "+e)});
+	incrementCounter(()=>res.sendFile(counterFile));})
+
 app.listen(80, ()=>console.log("Running at 80"))
 
 async function incrementCounter(cb){
